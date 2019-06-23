7dd7fafac8f098ee993c38250bb8e081dc4727a7
GraceToa <gracetoa@uoc.edu>
2019-06-23 12:56:23 +0200

    Initial Commit

---
+++ StoreAlmofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreAlmofire
+//
+//  Created by GraceToa on 23/06/2019.
+//  Copyright © 2019 GraceToa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
